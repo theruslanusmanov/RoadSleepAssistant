3ddd125a7f0641018a2bd9aa75f43b2bb543d9e0
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-18 22:51:05 +0500

    add preview for finish

--- app/src/main/java/com/github/theruslanusmanov/roadsleepassistant/Finish.kt
+++ app/src/main/java/com/github/theruslanusmanov/roadsleepassistant/Finish.kt
@@ -2,8 +2,15 @@ package com.github.theruslanusmanov.roadsleepassistant
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Finish() {
     Text(text = "Finish view")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FinishPreview() {
+    Finish()
 }
