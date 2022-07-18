70393e993f63fbd4562935efaa56c324b436abe7
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-07-18 22:51:23 +0500

    add map comp

---
+++ app/src/main/java/com/github/theruslanusmanov/roadsleepassistant/Map.kt
@@ -0,0 +1,16 @@
+package com.github.theruslanusmanov.roadsleepassistant
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Map() {
+    Text(text = "Map view")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MapPreview() {
+    Map()
+}
